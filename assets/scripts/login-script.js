75d628962c953a6c6e4ce6a4973c60595d84faf0
Reza Paramarta <rparamarta7@gmail.com>
2025-11-02 14:11:34 +0700

    Update login-script.js
    Change comment as advices with JSDoc format.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,51 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/**
+* Membuat variable loginFormELement untuk tampilan form.
+* @constant {HTMLlement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/**
+* Membuat variable inputEmailELement untuk tampilan input email.
+*@constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/**
+* Membuat variable inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
-
+/**
+* Membuat variable expectedEmail untuk menyimpan informasi email sementara.
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+/**
+* Membuat variable expectedPassword untuk menyimpan informasi password sementara.
+* @constant {string }
+*/
 const expectedPassword = 'superpassword';
-
+/* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-
+  /**
+  * Membuat variable email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+  * @constant {string}
+  */
   const email = inputEmailElement.value;
+  /**
+  * Membuat variable password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+  * @constant {string}
+  */
   const password = inputPasswordElement.value;
+   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+     /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
